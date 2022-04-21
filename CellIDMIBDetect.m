2e4cbff1f7c620ae06347e08cfa34300d128a308
Sebastian Obernberger <sebiobernberger@yahoo.de>
2022-04-21 14:49:11 -0700

    fix

--- CellIDMIBDetect.m
+++ CellIDMIBDetect.m
@@ -211,13 +211,10 @@ for frameNum = 1:(floor(ApproxNum2Frames/2)-1)
     
     % Decode PBCH
     [bchBits, pbchSymbols, nfmod4, mib, enb.CellRefP] = ltePBCHDecode(enb, pbchRx, pbchHest, nestsf); 
-%     if exist('scat', 'var')
-%         release(scat)
-%     end   
+
     release(scat)
     step(scat, pbchSymbols);
-%     drawnow;
-    % Parse MIB bits
+
     enb = lteMIB(mib, enb); 
     
 
@@ -236,12 +233,6 @@ for frameNum = 1:(floor(ApproxNum2Frames/2)-1)
         
     end
     
-%     meas2 = hRSMeasurements(enbcp, rxgrid);
-%     if MIBok
-%         fprintf(sep)
-%         fprintf('\n   MIB decoded')
-%         CorrectMIB{frameNum} = enb;
-%     end
 
     if CellIdOk && MIBok
         rxgridmib = lteOFDMDemodulate(enb, downsampled);
@@ -253,15 +244,7 @@ for frameNum = 1:(floor(ApproxNum2Frames/2)-1)
         MIBDecoded{end+1} = enb;
     end
 end
-% 
-% figure("Name",'RSRQdB')
-% plot(x, RSRQdB);
-% 
-% figure("Name",'RSRPdBm');
-% plot(x, RSRPdBm);
-% 
-% figure("Name",'RSSIdbm');
-% plot(x, RSSIdBm);
+
 
 
 Correlations = CellIds(:, 2)./CellIds(:, 3);
@@ -320,199 +303,3 @@ end
 
 
 
-% 
-% 
-% fprintf('Restarting reception now that bandwidth (NDLRB=%d) is known...\n',enb.NDLRB);
-% 
-% % Resample now we know the true bandwidth
-% ofdmInfo = lteOFDMInfo(enb);
-% if (sr~=ofdmInfo.SamplingRate)
-%     if (sr < ofdmInfo.SamplingRate)
-%         warning('The received signal sampling rate (%0.3fMs/s) is lower than the desired sampling rate for NDLRB=%d (%0.3fMs/s); PDCCH search / SIB1 decoding may fail.',sr/1e6,enb.NDLRB,ofdmInfo.SamplingRate/1e6);
-%     end    
-%     fprintf('\nResampling from %0.3fMs/s to %0.3fMs/s...\n',sr/1e6,ofdmInfo.SamplingRate/1e6);
-% else
-%     fprintf('\nResampling not required; received signal is at desired sampling rate for NDLRB=%d (%0.3fMs/s).\n',enb.NDLRB,sr/1e6);
-% end
-% nSamples = ceil(ofdmInfo.SamplingRate/round(sr)*size(waveform,1));
-% resampled = zeros(nSamples, nRxAnts);
-% for i = 1:nRxAnts
-%     resampled(:,i) = resample(waveform(:,i), ofdmInfo.SamplingRate, round(sr));
-% end
-% 
-% 
-% fprintf('\nPerforming frequency offset estimation...\n');
-% delta_f = lteFrequencyOffset(enb, resampled);
-% fprintf('Frequency offset: %0.3fHz\n',delta_f);
-% resampled = lteFrequencyCorrect(enb, resampled, delta_f);
-% 
-% % Find beginning of frame
-% ThreeBeginningFrames = resampled(1:3*10e-3*ofdmInfo.SamplingRate);
-% 
-% 
-% fprintf('\nPerforming timing offset estimation...\n');
-% offset = lteDLFrameOffset(enb, ThreeBeginningFrames); 
-% fprintf('Timing offset to frame start: %d samples\n',offset);
-% % Aligning signal with the start of the frame
-% resampled = resampled(1+offset:end,:);   
-% 
-% % OFDM demodulation
-% fprintf('\nPerforming OFDM demodulation...\n\n');
-% 
-% 
-% NumSamplesPerFrame = ofdmInfo.SamplingRate*10e-3;
-% tailSamples = mod(length(resampled),NumSamplesPerFrame);
-% resampled = resampled(1:end-tailSamples, :);
-% numFullFrames = length(resampled(:,1))/NumSamplesPerFrame;
-% 
-% LFrame = 10*L; % OFDM symbols per frame
-% 
-% rxgrid = lteOFDMDemodulate(enb, resampled);
-% 
-% 
-% if (mod(enb.NFrame,2)~=0)
-%     if (size(rxgrid,2)>=(L*10))
-%         rxgrid(:,1:(L*10),:) = [];
-%         fprintf('Skipping frame %d (odd frame number does not contain SIB1).\n\n',enb.NFrame);
-%     else
-%         rxgrid = [];
-%     end
-%     enb.NFrame = enb.NFrame + 1;
-% end
-% 
-% % Advance to subframe 5, or terminate if we have less than 5 subframes
-% if (size(rxgrid,2)>=(L*5))
-%     rxgrid(:,1:(L*5),:) = [];   % Remove subframes 0 to 4
-% else
-%     rxgrid = [];
-% end
-% enb.NSubframe = 5;
-% 
-% if (isempty(rxgrid))
-%     fprintf('Received signal does not contain a subframe carrying SIB1.\n\n');
-% end
-% 
-% % Reset the HARQ buffers
-% decState = [];
-% 
-% % While we have more data left, attempt to decode SIB1
-% while (size(rxgrid,2) > 0)
-% 
-%     fprintf('%s\n',sep);
-%     fprintf('SIB1 decoding for frame %d\n',mod(enb.NFrame,1024));
-%     fprintf('%s\n\n',sep);
-% 
-%     % Reset the HARQ buffer with each new set of 8 frames as the SIB1
-%     % info may be different
-%     if (mod(enb.NFrame,8)==0)
-%         fprintf('Resetting HARQ buffers.\n\n');
-%         decState = [];
-%     end
-% 
-%     % Extract current subframe
-%     rxsf = rxgrid(:,1:L,:);
-% 
-%     % Perform channel estimation
-%     [hestsf, nestsf] = lteDLChannelEstimate(enb, cec, rxsf);
-% 
-%     fprintf('Decoding CFI...\n\n');
-%     pcfichIndices = ltePCFICHIndices(enb);  
-%     [pcfichRx, pcfichHest] = lteExtractResources(pcfichIndices, rxsf, hestsf);
-%  
-%     cfiBits = ltePCFICHDecode(enb, pcfichRx, pcfichHest, nestsf);
-%     enb.CFI = lteCFIDecode(cfiBits); 
-% 
-%     
-%     fprintf('Decoded CFI value: %d\n\n', enb.CFI);
-% 
-%     if (strcmpi(enb.DuplexMode,'TDD'))
-%         tddConfigs = [1 6 0];
-%     else
-%         tddConfigs = 0;
-%     end
-%     dci = {};
-%     while (isempty(dci) && ~isempty(tddConfigs))
-%   
-%         if (strcmpi(enb.DuplexMode,'TDD'))
-%             enb.TDDConfig = tddConfigs(1);
-%         end
-%         tddConfigs(1) = [];
-%         pdcchIndices = ltePDCCHIndices(enb); % Get PDCCH indices
-%         [pdcchRx, pdcchHest] = lteExtractResources(pdcchIndices, rxsf, hestsf);
-%         
-%         [dciBits, pdcchSymbols] = ltePDCCHDecode(enb, pdcchRx, pdcchHest, nestsf);
-% 
-% 
-%         fprintf('PDCCH search for SI-RNTI...\n\n');
-%         pdcch = struct('RNTI', 65535);
-%         dci = ltePDCCHSearch(enb, pdcch, dciBits); % Search PDCCH for DCI
-%     end
-% 
-%     % If DCI was decoded, proceed with decoding PDSCH / DL-SCH
-%     if ~isempty(dci)
-% 
-%         dci = dci{1};
-%         fprintf('DCI message with SI-RNTI:\n');
-%         disp(dci);
-%         % Get the PDSCH configuration from the DCI
-%         [pdsch, trblklen] = hPDSCHConfiguration(enb, dci, pdcch.RNTI);
-%         pdsch.NTurboDecIts = 5;
-%         fprintf('PDSCH settings after DCI decoding:\n');
-%         disp(pdsch);
-% 
-%         % PDSCH demodulation and DL-SCH decoding to recover SIB bits.
-%         % The DCI message is now parsed to give the configuration of the
-%         % corresponding PDSCH carrying SIB1, the PDSCH is demodulated and
-%         % finally the received bits are DL-SCH decoded to yield the SIB1
-%         % bits.
-% 
-%         fprintf('Decoding SIB1...\n\n');
-%         % Get PDSCH indices
-%         [pdschIndices,pdschIndicesInfo] = ltePDSCHIndices(enb, pdsch, pdsch.PRBSet);
-%         [pdschRx, pdschHest] = lteExtractResources(pdschIndices, rxsf, hest);
-%         % Decode PDSCH
-%         [dlschBits,pdschSymbols] = ltePDSCHDecode(enb, pdsch, pdschRx, pdschHest, nestsf);
-%         % Decode DL-SCH with soft buffer input/output for HARQ combining
-%         if ~isempty(decState)
-%             fprintf('Recombining with previous transmission.\n\n');
-%         end
-%         [sib1, crc, decState] = lteDLSCHDecode(enb, pdsch, trblklen, dlschBits, decState);
-% 
-%         % Compute PDSCH EVM
-%         recoded = lteDLSCH(enb, pdsch, pdschIndicesInfo.G, sib1);
-%         remod = ltePDSCH(enb, pdsch, recoded);
-%         [~,refSymbols] = ltePDSCHDecode(enb, pdsch, remod);
-%         release(pdschEVM);
-%         [rmsevm,peakevm] = step(pdschEVM, refSymbols{1}, pdschSymbols{1});
-%         fprintf('PDSCH RMS EVM: %0.3f%%\n',rmsevm);
-%         fprintf('PDSCH Peak EVM: %0.3f%%\n\n',peakevm);
-% 
-%         fprintf('SIB1 CRC: %d\n',crc);
-%         if crc == 0
-%             fprintf('Successful SIB1 recovery.\n\n');
-%         else
-%             fprintf('SIB1 decoding failed.\n\n');
-%         end
-% 
-%     else
-%         % indicate that DCI decoding failed
-%         fprintf('DCI decoding failed.\n\n');
-%     end
-% 
-%     % Update channel estimate plot
-%     figure(channelFigure);
-%     surf(abs(hest(:,:,1,1)));
-%     hSIB1RecoveryExamplePlots(channelFigure);
-%     channelFigure.CurrentAxes.XLim = [0 size(hest,2)+1];
-%     channelFigure.CurrentAxes.YLim = [0 size(hest,1)+1];
-% 
-%     % Skip 2 frames and try SIB1 decoding again, or terminate if we
-%     % have less than 2 frames left.
-%     if (size(rxgrid,2)>=(L*20))
-%         rxgrid(:,1:(L*20),:) = [];   % Remove 2 more frames
-%     else
-%         rxgrid = []; % Less than 2 frames left
-%     end
-%     enb.NFrame = enb.NFrame+2;
-% 
-% end
